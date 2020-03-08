d32cd02c4b9528314f714cdbd22d50d75333ad85
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-08 23:59:48 +0100

    Add more

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        FileContent fileContent = new FileContent("resources/test.txt");
+//        WordIterator wordIterator = (WordIterator) fileContent.wordIterator();
+//
+//        int counter = 0;
+//        while(wordIterator.hasNext()) {
+//            System.out.println(wordIterator.next());
+//            counter++;
+//        }
+//
+//        System.out.println(counter);
+
+        System.out.println("\n\n\n---------------------------- Word");
+
+        StatisticalAnalysis statisticalAnalysis = new StatisticalAnalysis();
+
+        statisticalAnalysis.displayWordCount();
+        statisticalAnalysis.displayDictionarySize();
+
+        statisticalAnalysis.displayDictionary();
+    }
+}
